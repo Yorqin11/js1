d965571946094a6fb54908c2a592c1d31ffcc621
Yorqin11 <yorqinturgunboyev5@gmail.com>
2024-12-16 16:46:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = prompt('Ismingizni kiriting');
+const surname = prompt('Familyangizni kiriting'); 
+const age = prompt('Yoshingizni kiriting'); 
+const subject = prompt('Yoqtirgan faningizni kiriting'); 
+const street = prompt('Yashash manzilingizni kiriting'); 
+
+console.log(`ismingiz: ${name}`);
+console.log(`familyangiz: ${surname}`);
+console.log(`yoshingiz: ${age}`);
+console.log(`yoqtirgan faningiz: ${subject}`);
+console.log(`yashash manzilingiz ${street}`);
